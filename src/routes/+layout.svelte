87babc27f615b594b4d4731aebcd17d47b1b78af
Mazylol <landonp95@gmail.com>
2023-02-22 10:51:28 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+    import "../app.css"
+
+    const links = new Map([
+        ['Home', '/'],
+        ['Projects', '/projects'],
+        ['About', '/about']
+    ])
+</script>
+
+<div class="bg-neutral-700 min-h-screen">
+    <nav class="w-full bg-neutral-800">
+        <div class="flex flex-row ml-8">
+            {#each [...links] as [title, url]}
+                <div>
+
+                </div>
+            {/each}
+        </div>
+    </nav>
+    <slot />
+</div>
